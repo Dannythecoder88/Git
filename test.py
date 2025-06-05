f084a36a6ebcbcc0efe34cf4d78579025ae0f2d3
Dannythecoder <daniel.joseph666666@gmail.com>
2025-06-04 23:24:06 -0400

    Debug and called functions

--- test.py
+++ test.py
@@ -1,27 +1,33 @@
-from JES import *
-from math import*
-from random import*
+from random import *
 
 def start_game():
-    Correct = random.randit(0,100)
-    guesses = {}
+    target = randint(1, 100)  
+    guesses = []              
+    print("Guess the number from 1-100. You have 7 tries.")
 
-    for attempt in range ((1,8)):
-        get_guess(attempt)
+    for attempt in range(1, 8):  
+        guess = get_guess(attempt)
         guesses.append(guess)
-    if right_guess(guess, target_):
-        print("Congrats, you guessed correctly")
+
+        if check_guess(guess, target):
+            print("Congrats, you guessed correctly!")
+            break
     else:
-        print("Sorry, you are all out of guesses")
+        print("Sorry, you're out of guesses. The number was:", target)
+
+    print("Your guesses were:", guesses)
 
 def get_guess(try_number):
-    guess = input(f"Try {try_number}: Enter a number: ")
-    if guess.isdigit():
-        guess = int(guess)
-        if 1 <= guess <= 100:
-            return guess
+    while True:
+        guess = input(f"Try {try_number}: Enter a number: ")
+        if guess.isdigit():
+            guess = int(guess)
+            if 1 <= guess <= 100:
+                return guess
+            else:
+                print("Number must be between 1 and 100.")
         else:
-            print("Number must be between 1 and 100.")
+            print("Please enter a valid number.")
 
 def check_guess(guess, number):
     if guess < number:
@@ -32,3 +38,5 @@ def check_guess(guess, number):
         return False
     else:
         return True
+
+start_game()
--- test2.py
+++ test2.py
@@ -1,22 +1,20 @@
-from JES import *
-from math import*
 from random import*
 
 def fruit_game():
-    guess = [apple,pear,banana]
-    word = random.choice(words)
+    guess = ["apple","pear","banana"]
+    word = choice(guess)
     tries = 6
-print("Guess fruit")
-print("You have 3 tries:")
+    print("Guess fruit")
+    print("You have 3 tries:")
+    while tries >0:
+        guess = input("Enter your frut")
+        if guess == word:
+            print("Correct, Good Job")
+        else:
+            print("You are wrong")
+            tries = tries -1
+    if tries == 0:
+        print("You lost")
 
-while tries >0:
-    guess = input("Enter your frut")
-if guess == word:
-    print("Correct, Good Job")
-else:
-    print("You are wrong")
-    tries = tries -1
 
-if tries == 0:
-    print("You lost")
-
+fruit_game()
